2f99acf0845c32bdb7eea24af731e426691f5502
Letur <LeturCZ@seznam.cz>
2025-09-17 00:01:36 +0200

    (FMT) expand XDG option

--- users/letur/home.nix
+++ users/letur/home.nix
@@ -15,28 +15,30 @@ in {
     };
   };
 
-  xdg.desktopEntries.codium = {
-    name = "VSCodium";
-    genericName = "Text Editor";
-    comment = "Code Editing. Redefined.";
-    exec = "codium --enable-features=UseOzonePlatform --ozone-platform=wayland %F";
-    terminal = false;
-    categories = [
-      "Utility"
-      "TextEditor"
-      "Development"
-      "IDE"
-    ];
-    icon = "vscodium";
-    type = "Application";
-    settings = {
-      StartupWMClass = "vscodium";
-      StartupNotify = "true";
-      Keywords = "vscode";
-    };
-    actions.new-empty-window = {
-      name = "New Empty Window";
-      exec = "codium --new-window --enable-features=UseOzonePlatform --ozone-platform=wayland %F";
+  xdg = {
+    desktopEntries.codium = {
+      name = "VSCodium";
+      genericName = "Text Editor";
+      comment = "Code Editing. Redefined.";
+      exec = "codium --enable-features=UseOzonePlatform --ozone-platform=wayland %F";
+      terminal = false;
+      categories = [
+        "Utility"
+        "TextEditor"
+        "Development"
+        "IDE"
+      ];
+      icon = "vscodium";
+      type = "Application";
+      settings = {
+        StartupWMClass = "vscodium";
+        StartupNotify = "true";
+        Keywords = "vscode";
+      };
+      actions.new-empty-window = {
+        name = "New Empty Window";
+        exec = "codium --new-window --enable-features=UseOzonePlatform --ozone-platform=wayland %F";
+      };
     };
   };
 
