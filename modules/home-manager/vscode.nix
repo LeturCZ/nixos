058e8b54461737d2fdeab06bc8a2a40561a8410d
Letur <LeturCZ@seznam.cz>
2025-09-19 20:56:48 +0200

    vscode: Disable AI features by default

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -1,30 +1,60 @@
-{ lib, config, pkgs, ...}:
-with lib; {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+{
   options = {
     home-manager.users = lib.mkOption {
-      type = types.attrsOf (types.submoduleWith {
-        modules = toList ({name, ...}: let
-          cfg = config.home-manager.users.${name};
-        in {
-          options.programs.vscode = {
-            startupArguments = mkOption {
-              type = types.attrs;
-              default = {
-                enable-crash-reporter = false;
+      type = types.attrsOf (
+        types.submoduleWith {
+          modules = toList (
+            { name, ... }:
+            let
+              cfg = config.home-manager.users.${name};
+            in
+            {
+              options.programs.vscode = {
+                startupArguments = mkOption {
+                  type = types.attrs;
+                  default = {
+                    enable-crash-reporter = false;
+                  };
+                  description = "Set permanent commandline arguments for vscode";
+                };
+                disableAIFeatures = mkOption {
+                  type = types.bool;
+                  default = true;
+                };
+                profiles = mkOption {
+                  type = types.attrsOf (
+                    types.submoduleWith {
+                      shorthandOnlyDefinesConfig = true;
+                      modules = toList (
+                        { name, ... }:
+                        {
+                          config.userSettings."chat.disableAIFeatures" = mkIf cfg.programs.vscode.disableAIFeatures false;
+                        }
+                      );
+                    }
+                  );
+                };
               };
-              description = "Set permanent commandline arguments for vscode";
-            };
-          };
-          config = mkIf cfg.programs.vscode.enable {
-            home.file.vscodeArgv = {
-              enable = true;
-              force = true;
-              target = ".vscode-oss/argv.json";
-              text = builtins.toJSON cfg.programs.vscode.startupArguments;
-            };
-          };
-        });
-      });
+              config = mkIf cfg.programs.vscode.enable {
+                home.file.vscodeArgv = {
+                  enable = true;
+                  force = true;
+                  target = ".vscode-oss/argv.json";
+                  text = builtins.toJSON cfg.programs.vscode.startupArguments;
+                };
+
+              };
+            }
+          );
+        }
+      );
     };
   };
 }
