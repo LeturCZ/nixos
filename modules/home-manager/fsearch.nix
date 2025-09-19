ae94d139dc00cd8529bb9e88ac067c7051120d1c
Letur <LeturCZ@seznam.cz>
2025-09-19 11:33:25 +0200

    Update fsearch options, use lib INI writer instead of custom function

--- modules/home-manager/fsearch.nix
+++ modules/home-manager/fsearch.nix
@@ -2,43 +2,53 @@
   config,
   lib,
   pkgs,
-  customUtils,
   ...
 }:
-with lib; {
+with lib;
+{
   options = {
     home-manager.users = lib.mkOption {
-      type = types.attrsOf (types.submoduleWith {
-        modules = toList ({name, ...}: let
-          cfg = config.home-manager.users.${name};
-        in {
-          options.programs.fsearch = {
-            enable = mkEnableOption "enable Fsearch for user";
-            darkTheme = mkOption {
-              type = types.bool;
-              default = true;
-            };
-          };
-          config = mkIf cfg.programs.fsearch.enable {
-            home.packages = [pkgs.fsearch];
-            home.file.fsearchConfig = {
-              enable = true;
-              force = true;
-              target = ".config/fsearch/fsearch.conf";
-              text = customUtils.toConf {
-                Interface.enable_dark_theme = true;
-                Database = {
-                  location_1 = "/";
-                  location_enabled_1 = true;
-                  location_one_filesysyem = false;
-                  exclude_location_1 = "/proc";
-                  exclude_location_enabled_1 = true;
+      type = types.attrsOf (
+        types.submoduleWith {
+          modules = toList (
+            { name, ... }:
+            let
+              cfg = config.home-manager.users.${name};
+            in
+            {
+              options.programs.fsearch = {
+                enable = mkEnableOption "enable Fsearch for user";
+                darkTheme = mkOption {
+                  type = types.bool;
+                  default = true;
                 };
               };
-            };
-          };
-        });
-      });
+              config = mkIf cfg.programs.fsearch.enable {
+                home.packages = [ pkgs.fsearch ];
+                home.file.fsearchConfig = {
+                  enable = true;
+                  force = true;
+                  target = ".config/fsearch/fsearch.conf";
+                  text = lib.generators.toINI { } {
+                    Interface.enable_dark_theme = true;
+                    Database = {
+                      update_database_on_launch = true;
+                      update_database_every_hours = 0;
+                      update_database_every_minutes = 15;
+                      location_1 = "/";
+                      location_update_1 = true;
+                      location_enabled_1 = true;
+                      location_one_filesysyem_1 = false;
+                      exclude_location_1 = "/proc";
+                      exclude_location_enabled_1 = true;
+                    };
+                  };
+                };
+              };
+            }
+          );
+        }
+      );
     };
   };
 }
